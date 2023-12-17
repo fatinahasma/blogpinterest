5c961bd952a9019df9bbbee7e627dcaef419b29b
fatinahasma <154037911+fatinahasma@users.noreply.github.com>
2023-12-17 12:04:47 +0700

    Create adsb300.js

---
+++ adsb300.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '2ece564fbf822dc0e9447946b67d03e3',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//careersadorable.com/2ece564fbf822dc0e9447946b67d03e3/invoke.js"></scr' + 'ipt>');
